bf9003d7593343d955313fa721d40f4294a90584
Brady Mann <Football3320>
2019-02-14 13:52:35 -0500

    added and declared the Scanner

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,10 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner keyboard;
     }
 }
