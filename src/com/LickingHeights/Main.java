a0a7ca36478671adc759bdde98d66ab0feb31749
Brady Mann <Football3320>
2019-02-19 13:48:43 -0500

    added conditionals when the number is less than or equal to 10, 20, 30

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -17,6 +17,15 @@ public class Main {
         question = keyboard.nextLine();
         System.out.println("Now, give a number between 1 and 100");
         testing = keyboard.nextInt();
-       
-            }
+        if (testing<=10) {
+            System.out.println("Sure thing!");
+        }
+        else if (testing<=20) {
+            System.out.println("Not so sure about that...");
+        }
+        else if (testing<=30) {
+            System.out.println("Not today..");
+        }
+
+    }
 }
